a1fd4d3cd2915a3bf681a32497b24f21c73ceea5
Jakly Medalla <medallacreate@gmail.com>
2026-07-13 22:44:19 -0700

    Authentication Progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 import Navbar from './components/Navbar'
+import AuthProvider from './context/AuthContext'
 
 function App() {
   
   return (
+  <AuthProvider>
   <div className='app'>
     <Navbar />
     <Routes>
@@ -16,6 +18,7 @@ function App() {
       <Route path='/checkout' element={<Checkout/>} />
     </Routes>
   </div>
+  </AuthProvider>
   );
 }
 
